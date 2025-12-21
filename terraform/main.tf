be0ab44ba563b12b97c00da56de79465a70604d2
Louis Amoah-Nuamah <louisan42@gmail.com>
2025-12-21 16:41:43 -0500

    update provider version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"
-      version = "3.0.1"
+      source  = "Telmate/proxmox"
+      version = "~> 3.0.2-rc07"
     }
   }
 }
