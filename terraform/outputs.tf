e8b04aada21a72ed3f854c4540fb7efb8e93c735
Louis Amoah-Nuamah <louisan42@gmail.com>
2025-12-30 23:00:30 -0500

    fix: use Terraform-generated hostname for FreeIPA instead of app_name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,3 +16,8 @@ output "vm_target_ip" {
   value = var.resource_type == "vm" ? var.vm_target_ip : null
   description = "The static IP address configured for the VM via cloud-init"
 }
+
+output "vm_hostname" {
+  value = local.vm_hostname
+  description = "The generated hostname for the VM or LXC container"
+}
